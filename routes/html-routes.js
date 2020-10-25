5506956fb5482eb47eef694cb3dd51bb79ceba73
Sarah Seagraves <67175494+seagraving@users.noreply.github.com>
2020-10-25 10:33:37 -0500

    added server.js file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,14 +1,17 @@
 var path = require("path");
 const router = require("express").Router()
 
-
+ //sends to exercise
   router.get("/exercise", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
+
+  //sends to homepage
   router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
+  //sends to stats page
   router.get("/stats", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
